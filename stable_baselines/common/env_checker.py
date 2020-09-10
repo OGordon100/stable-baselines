963967cb8e7b3c2524c534dbfa68873699f8861b
OGordon100 <35759308+OGordon100@users.noreply.github.com>
2020-09-10 17:26:21 +0100

    Fix typo in env_checker

--- stable_baselines/common/env_checker.py
+++ stable_baselines/common/env_checker.py
@@ -178,7 +178,7 @@ def check_env(env: gym.Env, warn: bool = True, skip_render_check: bool = True) -
     :param skip_render_check: (bool) Whether to skip the checks for the render method.
         True by default (useful for the CI)
     """
-    assert isinstance(env, gym.Env), ("You environment must inherit from gym.Env class "
+    assert isinstance(env, gym.Env), ("Your environment must inherit from gym.Env class "
                                       " cf https://github.com/openai/gym/blob/master/gym/core.py")
 
     # ============= Check the spaces (observation and action) ================
